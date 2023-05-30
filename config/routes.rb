2a1b8a2dd3e5a253b22ef31c092cc2f699560585
Georg Grimm <gjgrimm@gmx.de>
2023-05-30 14:26:57 +0200

    Done it with out styling it

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/tasks", to: "tasks#index", as: :tasks
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  get "tasks/new", to: "tasks#new"
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
+  root to: "tasks#index"
+  patch "tasks/:id", to: "tasks#update", as: :update
+  delete "tasks/:id", to: "tasks#destroy"
 end
